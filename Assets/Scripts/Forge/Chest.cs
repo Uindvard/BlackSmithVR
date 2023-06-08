460f37175ad6a5c86c8013d8c28f156c52a70ac4
Uindvard <135325781+Uindvard@users.noreply.github.com>
2023-06-08 12:20:37 +0300

    Newww
    цц

--- Assets/Scripts/Forge/Chest.cs
+++ Assets/Scripts/Forge/Chest.cs
@@ -1,26 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Chest: MonoBehaviour
+public class ChestInteraction : MonoBehaviour
 {
-    public GameObject chestUI; // Ссылка на UI-панель с ячейками инвентаря
-    private bool isOpen = false; // Флаг, указывающий, открыт ли сундук
+    public GameObject uiPanel;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Проверяем, если касание произошло с коллайдером контроллера
         if (other.CompareTag("Controller"))
         {
-            // Проверяем, если нажата кнопка активации на контроллере
-            if (Input.GetButtonDown("XRI_LeftHand_Interaction_Activate_Value"))
-            {
-                // Переключаем состояние сундука (открыт/закрыт)
-                isOpen = !isOpen;
-
-                // Показываем или скрываем UI-панель в зависимости от состояния сундука
-                chestUI.SetActive(isOpen);
-            }
+            uiPanel.SetActive(true);
         }
     }
 }
